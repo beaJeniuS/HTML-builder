c012f24f8c2b24dc379a4c02d6cd0d4255ae3e8f
beaJenius <beajenius@yandex.ru>
2022-05-22 13:00:33 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+const srcFolder = path.join(__dirname, 'files');
+const destFolder = path.join(__dirname, 'files-copy');
+
+function copyFolder(srcFolder, destFolder) {
+  try {
+    fs.mkdir(destFolder, { recursive: true }, (error) => {
+      if (error) {
+        throw error;
+      } 
+      fs.readdir(srcFolder, { withFileTypes: true }, (error, fileList) => {
+        if (error) throw error;
+        fileList.forEach(file => {
+          if (file.isFile()) {
+            fs.copyFile(path.join(srcFolder, file.name), path.join(destFolder, file.name), () => {});
+          } else {
+            copyFolder(path.join(srcFolder,file.name));
+          }
+        });
+      });
+    });  
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyFolder(srcFolder, destFolder);
 
