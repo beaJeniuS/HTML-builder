96333ebca65243014ee2023a3c89f16887b29c62
beaJenius <beajenius@yandex.ru>
2022-05-23 23:49:05 +0300

    feat: change bundle function logic

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const srcFolder = path.join(__dirname);
 const destFolder = path.join(__dirname, 'project-dist');
 
+let templateText = '';
+
 function copyFolder(srcFolder, destFolder) {
   try {
     fs.mkdir(destFolder, { recursive: true }, (error) => {
@@ -26,34 +28,38 @@ function copyFolder(srcFolder, destFolder) {
   }
 }
 
-fs.mkdir(destFolder, { recursive: true }, (err) => {
+fs.rm(destFolder, { recursive: true, force: true }, (err) => {
   if (err) throw err;
-});
 
-fs.mkdir(path.join(srcFolder, 'assets'), { recursive: true }, (err) => {
-  if (err) throw err;
-});
+  fs.mkdir(destFolder, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
-fs.mkdir(path.join(destFolder, 'assets'), { recursive: true }, (err) => {
-  if (err) throw err;
-});
+  fs.mkdir(path.join(srcFolder, 'assets'), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
-copyFolder(path.join(srcFolder, 'assets'), path.join(destFolder,'assets'));
+  fs.mkdir(path.join(destFolder, 'assets'), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 
-// ----- opening template.html
-let templateText = '';
-const readStream = fs.createReadStream(path.join(srcFolder, 'template.html'));
+  copyFolder(path.join(srcFolder, 'assets'), path.join(destFolder, 'assets'));
 
-readStream.on('data', (chunk) => {
-  templateText += chunk.toString();
-});
+  const readStream = fs.createReadStream(path.join(srcFolder, 'template.html'));
 
-readStream.on('error', (error) => {
-  console.log('Произошла ошибка: ', error.message);
-});
+  readStream.on('data', (chunk) => {
+    templateText += chunk.toString();
+  });
 
-readStream.on('end', () => {
-  createHtmlByTemplate(templateText);
+  readStream.on('error', (error) => {
+    console.log('Произошла ошибка: ', error.message);
+  });
+
+  readStream.on('end', () => {
+    createHtmlByTemplate(templateText);
+  });  
+
+  makeBundle(path.join(srcFolder,'styles'), destFolder);
 });
 
 function createHtmlByTemplate(templateText) {
@@ -84,24 +90,35 @@ function createHtmlByTemplate(templateText) {
 }
 
 function parseTemplate(templateText) {
-  return templateText.match(/{{[a-zA-Z]*}}/g).map((el) => el.replace(/{{/g, '').replace(/}}/g, ''));
+  return templateText.match(/{{\s*[a-zA-Z]*\s*}}/g).map((el) => el.replace(/{{/g, '').replace(/}}/g, '').trim());
 }
 
 function makeBundle(srcFolder, destFolder) {
+  let files = [];
   fs.createWriteStream(path.join(destFolder, 'style.css')).end();
   fs.readdir(srcFolder, { withFileTypes: true }, (error, fileList) => {
     if (error) throw error;
     fileList.forEach(file => { 
       if (path.parse(file.name).ext.slice(1) === 'css') {
-        const readStream = fs.createReadStream(path.join(srcFolder, file.name));
-        readStream.on('data', (chunk) => {
-          fs.appendFile(path.join(destFolder, 'style.css'), chunk.toString(), (err) => {
-            if (err) throw err;
-          });
-        });        
+        files.push(fs.promises.readFile(path.join(srcFolder, file.name)));
       }
     });
+    
+    Promise.all(files).then(data => {
+      const writeStream = fs.createWriteStream(path.join(destFolder, 'style.css'));
+
+      let tmpStr = '';
+      data.forEach(element => {
+        tmpStr += element.toString() + '\n\n';
+      });
+
+      writeStream.write(tmpStr, (err) => {
+        if (err) throw err;
+      });
+
+      writeStream.end();
+    }).catch(err => {
+      console.log('Произошла ошибка: ', err);
+    });    
   });
 }
-
-makeBundle(path.join(srcFolder,'styles'), destFolder);
