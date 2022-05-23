da918fbb53f285893cc5086c874386b903b78cfd
beaJenius <beajenius@yandex.ru>
2022-05-23 23:50:31 +0300

    docs: delete comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,23 +4,6 @@ const fs = require('fs');
 const srcFolder = path.join(__dirname, 'styles');
 const destFolder = path.join(__dirname, 'project-dist');
 
-/*function makeBundle(srcFolder, destFolder) {
-  fs.createWriteStream(path.join(destFolder, 'bundle.css')).end();
-  fs.readdir(srcFolder, { withFileTypes: true }, (error, fileList) => {
-    if (error) throw error;
-    fileList.forEach(file => { 
-      if (path.parse(file.name).ext.slice(1) === 'css') {
-        const readStream = fs.createReadStream(path.join(srcFolder, file.name));
-        readStream.on('data', (chunk) => {
-          fs.appendFile(path.join(destFolder, 'bundle.css'), chunk.toString(), (err) => {
-            if (err) throw err;
-          });
-        });        
-      }
-    });
-  });
-}*/
-
 function makeBundle(srcFolder, destFolder) {
   let files = [];
   fs.createWriteStream(path.join(destFolder, 'bundle.css')).end();
